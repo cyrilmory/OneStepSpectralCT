2d43aceca4fccfd80ceadb59a977fd89a18536dd
Cyril Mory <cyril.mory@gmail.com>
2018-04-05 14:11:04 +0200

    Updated PlayIteratesTransposed to include scaling with material's density

--- Spectral/Utilities/PlayIteratesTransposed.m
+++ Spectral/Utilities/PlayIteratesTransposed.m
@@ -3,17 +3,19 @@ function [] = PlayIteratesTransposed(iterates,mat_inf, mat_sup)
 ObjectSize = sqrt(ObjectSize);
 
 % Normalize each material to uint8
-mat_rescaled = uint8(zeros(size(iterates)));
+tmp = iterates;
+tmp(:,1,:) = tmp(:,1,:) * 4.933;
+tmp(:,2,:) = tmp(:,2,:) * 7.9;
+
+mat_rescaled = zeros(size(iterates));
 for mat=1:nmat
-    mat_rescaled(:,mat,:) = uint8((iterates(:,mat,:) - mat_inf(mat)) * 255 / (mat_sup(mat) - mat_inf(mat)));
+    mat_rescaled(:,mat,:) = (tmp(:,mat,:) - mat_inf(mat)) / (mat_sup(mat) - mat_inf(mat));
 end
 
 % Play sequences side by side
-tmp=permute(reshape(mat_rescaled, [ObjectSize, ObjectSize, nmat, niterates]), [2 1 3 4]);
-% tmp=flip(tmp,1);
-% tmp=flip(tmp,2);
+mat_rescaled=permute(reshape(mat_rescaled, [ObjectSize, ObjectSize, nmat, niterates]), [2 1 3 4]);
 
-implay(reshape(tmp, [ObjectSize, ObjectSize*nmat, niterates]));
+implay(reshape(mat_rescaled, [ObjectSize, ObjectSize*nmat, niterates]));
 
 % % Write it
 % writeVideoResult(double(reshape(mat_rescaled, [ObjectSize, ObjectSize * nmat, niterates])), 'WeidingerNoRegul256_2000iter.avi', 0,255);
